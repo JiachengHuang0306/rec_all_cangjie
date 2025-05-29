e5693f7d2c2dceac84b22b788e9ed14769313f2f
jiachenghuang <hjiacheng0306@gmail.com>
2025-05-29 15:37:31 +0800

    final commit

---
+++ src/contrib/masked_text_list_api/db/030_create_maskedtextlistdb.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE maskedTextListDb;
+
+USE maskedTextListDb;
+
+CREATE TABLE maskedTextItem (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    itemId BIGINT NULL DEFAULT NULL,
+    content TEXT NOT NULL,
+    maskedContent TEXT NOT NULL,
+    userIdentityGuid VARCHAR(255) NOT NULL,
+    isDeleted TINYINT(1) NOT NULL DEFAULT 0,
+    INDEX idx_itemId (itemId),
+    INDEX idx_userIdentityGuid (userIdentityGuid),
+    INDEX idx_isDeleted (isDeleted)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
